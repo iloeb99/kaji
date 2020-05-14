0d7620643c3963e02491097318443af1f783324e
iloeb99 <iantkl@hotmail.com>
2020-05-13 17:31:00 -0700

    bug fix

--- irgen.ml
+++ irgen.ml
@@ -230,7 +230,7 @@ let translate (globals, functions) =
          let start' = build_expr builder start in
          let stop' = build_expr builder stop in
          L.build_call subStr [| start' ; stop' ; e|] "" builder
-      | SCall ("concatStr", [e1 ; e2]) ->
+      | SCall ("concatStr", [e ; e2]) ->
          let e1' = build_expr builder e1 in
          let e2' = build_expr builder e2 in
          L.build_call concatStr [| e1' ; e2' |] "c_ret" builder
